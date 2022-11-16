69459c408ae5183d557a9d0d56726d60a91e900f
nikita <zherko.na@phystech.edu>
2022-11-16 12:54:40 +0300

    add Example 005 If/else

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
